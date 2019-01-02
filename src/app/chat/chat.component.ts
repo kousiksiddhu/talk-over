38961bb77ea463bd194879e396094d2f8e8ed75d
Siddhu Nallasivam <kousiksiddhu@gmail.com>
2019-01-02 08:16:50 +0530

    share feature and anonymous user

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,9 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFirestore, AngularFirestoreDocument, CollectionReference } from 'angularfire2/firestore';
-import { Observable } from 'rxjs';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+  CollectionReference
+} from "angularfire2/firestore";
+import { Observable } from "rxjs";
+import { ActivatedRoute, Params } from "@angular/router";
+import randomProfile from "random-profile-generator";
 
-export interface Conversation { 
+export interface Conversation {
   title: string;
   description: string;
   message: Object;
@@ -13,6 +18,7 @@ export interface User {
   userId: number;
   userName: string;
   userIcon: any;
+  continuousMsg: boolean;
 }
 
 export interface Messages {
@@ -21,75 +27,91 @@ export interface Messages {
 }
 
 @Component({
-  selector: 'app-chat',
-  templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  selector: "app-chat",
+  templateUrl: "./chat.component.html",
+  styleUrls: ["./chat.component.css"]
 })
 export class ChatComponent implements OnInit {
-  
   document: AngularFirestoreDocument<Conversation>;
   conversations: Observable<Conversation>;
   getRouteId;
-  uniqueID:string;
+  uniqueID: string;
   user: User;
   messages;
 
-  constructor(private db: AngularFirestore, private activatedRoute: ActivatedRoute) {}
+  constructor(
+    private db: AngularFirestore,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    let profile = randomProfile.profile();
     this.getRouteId = this.activatedRoute.params.subscribe(params => {
-      this.uniqueID=params['id'];
+      this.uniqueID = params["id"];
     });
 
-    this.document = this.db.doc<Conversation>('conversations/' + this.uniqueID);
+    this.document = this.db.doc<Conversation>("conversations/" + this.uniqueID);
     this.conversations = this.document.valueChanges();
-    this.messages = this.document.collection('messages',ref => ref.orderBy('timestamp')).valueChanges();
-    
-    this.messages.subscribe((value)=>{
-      if(value && value.length && value[value.length - 1].user == this.user.userId){
-        setTimeout(()=>{
+    this.messages = this.document
+      .collection("messages", ref => ref.orderBy("timestamp"))
+      .valueChanges();
+
+    this.messages.subscribe(value => {
+      if (
+        value &&
+        value.length &&
+        value[value.length - 1].user == this.user.userId
+      ) {
+        this.user.continuousMsg = true;
+        setTimeout(() => {
           this.scrollToBottom();
         });
       }
-    })
+    });
+    console.log(profile);
 
     this.user = {
-      userId: Math.floor(Math.random() * 100),
-      userName: "",
-      userIcon: ""
-    }
+      userId: profile.id,
+      userName: profile.fullName,
+      userIcon: profile.avatar,
+      continuousMsg: false
+    };
   }
 
-  sendMessage(message: HTMLInputElement){
-    
-    if(!message.value){
+  sendMessage(message: HTMLInputElement) {
+    if (!message.value) {
       return false;
     }
 
     let msg = {
       user: this.user.userId,
+      userName: this.user.userName,
+      userIcon: this.user.userIcon,
+      continuousMsg: this.user.continuousMsg,
       message: message.value,
       timestamp: new Date()
-    }
-    
-    this.document.collection('messages')
+    };
+
+    console.log(this.user);
+    this.document
+      .collection("messages")
       .add(msg)
       .catch(error => {
         alert("Error Occured");
         console.log(error);
-      }).then(response => {
+      })
+      .then(response => {
         console.log(response);
       });
-    message.value="";
+    message.value = "";
     return false;
   }
 
   scrollToBottom() {
-    window.scrollTo(0,document.body.scrollHeight);              
+    window.scrollTo(0, document.body.scrollHeight);
   }
 
   ngOnDestroy() {
     this.getRouteId.unsubscribe();
   }
 }
-
--- src/app/getting-started/getting-started.component.ts
+++ src/app/getting-started/getting-started.component.ts
@@ -1,67 +1,82 @@
-import { RouterModule, RouterState } from '@angular/router';
-import { PlatformLocation } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
-import { Observable } from 'rxjs';
+import { RouterModule, RouterState } from "@angular/router";
+import { PlatformLocation } from "@angular/common";
+import { Component, OnInit } from "@angular/core";
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument
+} from "angularfire2/firestore";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'getting-started',
-  templateUrl: './getting-started.component.html',
-  styleUrls: ['./getting-started.component.css']
+  selector: "getting-started",
+  templateUrl: "./getting-started.component.html",
+  styleUrls: ["./getting-started.component.css"]
 })
-
 export class GettingStartedComponent implements OnInit {
-  conversationUrl:string;
+  conversationUrl: string;
   collection: AngularFirestoreCollection;
   document: AngularFirestoreDocument;
   UID: string;
-  constructor(private db: AngularFirestore, private platformLocation: PlatformLocation) {
-    this.collection = db.collection('conversations');  
+  constructor(
+    private db: AngularFirestore,
+    private platformLocation: PlatformLocation
+  ) {
+    this.collection = db.collection("conversations");
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  generateAnonymousLink(title:HTMLInputElement, description:HTMLInputElement) {
+  generateAnonymousLink(
+    title: HTMLInputElement,
+    description: HTMLInputElement
+  ) {
     this.UID = this.getUID();
 
-    this.collection.doc(this.UID).set({
-      title: title.value || "Start Chatting",
-      description: description.value || "This is an anonymous chat platform. User identity will not be revealed.",
-      message: []
-    }).catch(error => {
-      alert("Error Occured");
-    }).then(response => {
-      console.log(response);
-      let baseUrl = (this.platformLocation as any).location.href;
+    this.collection
+      .doc(this.UID)
+      .set({
+        title: title.value || "Start Chatting",
+        description:
+          description.value ||
+          "This is an anonymous chat platform. User identity will not be revealed.",
+        message: []
+      })
+      .catch(error => {
+        alert("Error Occured");
+      })
+      .then(response => {
+        console.log(response);
+        let baseUrl = (this.platformLocation as any).location.href;
 
-      if(baseUrl){
-        this.conversationUrl = baseUrl + "chat/" + this.UID;
-      }
-    });
+        if (baseUrl) {
+          this.conversationUrl = baseUrl + "chat/" + this.UID;
+          setTimeout(() => {
+            window.dispatchEvent(new Event("resize"));
+          });
+        }
+      });
   }
 
-  getUID(){
+  getUID() {
     let date = new Date();
     let components = [
-        date.getFullYear(),
-        date.getMonth(),
-        date.getDate(),
-        date.getHours(),
-        date.getMinutes(),
-        date.getSeconds(),
-        date.getMilliseconds()
+      date.getFullYear(),
+      date.getMonth(),
+      date.getDate(),
+      date.getHours(),
+      date.getMinutes(),
+      date.getSeconds(),
+      date.getMilliseconds()
     ];
 
     return components.join("");
   }
 
-  copyConversationUrl(conversationUrlInput:HTMLInputElement){
+  copyConversationUrl(conversationUrlInput: HTMLInputElement) {
     conversationUrlInput.select();
-    document.execCommand('copy');
+    document.execCommand("copy");
   }
 
-  openDialog(): void {
-
-  }
+  openDialog(): void {}
 }
